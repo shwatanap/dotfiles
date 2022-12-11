c30af5270010dc5c5542d9524870b1558356f1c5
shwatanap <watanabeshou1520@gmail.com>
2022-12-11 18:22:42 +0900

    :sparkles: gitの設定追加

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+## シンボリックリンク作成
+# zshrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+# git
+ln -s ~/dotfiles/git/.gitignore_global ~/.config/git/ignore
+ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
